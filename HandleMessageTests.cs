cae37f95055dfcb6b817ad54286b4b025651c93c
Matt Baker <baker.mattc@gmail.com>
2016-12-24 16:43:54 -0700

    initial commit

---
+++ HandleMessageTests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+
+namespace CharlesBukowskiSlackBot
+{
+    interface IGetRandomBukowskiQuote
+    {
+        string Execute();
+    }
+
+    class TestableGetRandomBukowskiQuote : IGetRandomBukowskiQuote
+    {
+        public bool ExecuteCalled { get; set; }
+        public string ExecuteReturnValue { get; set; }
+        public string Execute()
+        {
+            ExecuteCalled = true;
+            return ExecuteReturnValue;
+        }
+    }
+
+    interface ISendSlackMessage
+    {
+        void Execute(string channel, string message);
+    }
+
+    class TestableSendSlackMessage : ISendSlackMessage
+    {
+        public string ExecuteCalledWithChannel { get; set; }
+        public string ExecuteCalledWithMessage { get; set; }
+        public bool ExecuteCalled { get; set; }
+
+        public void Execute(string channel, string message)
+        {
+            ExecuteCalledWithChannel = channel;
+            ExecuteCalledWithMessage = message;
+            ExecuteCalled = true;
+        }
+    }
+
+    class MessageHandler
+    {
+        private IGetRandomBukowskiQuote getRandomBukowskiQuote;
+        private ISendSlackMessage sendSlackMessage;
+        private string botName;
+
+        public MessageHandler(IGetRandomBukowskiQuote getRandomBukowskiQuote, ISendSlackMessage sendSlackMessage, string botName)
+        {
+            this.botName = botName;
+            this.getRandomBukowskiQuote = getRandomBukowskiQuote;
+            this.sendSlackMessage = sendSlackMessage;
+        }
+
+        public void Handle(ConsoleApplication.Program.IncomingMessage message)
+        {
+            if (message.text.Contains($"<@{botName}>"))
+            {
+                var quote = this.getRandomBukowskiQuote.Execute();
+                this.sendSlackMessage.Execute(message.channel, quote);
+            }
+        }
+    }
+
+    [TestFixture]
+    public class HandleMessageTests
+    {
+        [Test]
+        public void when_handing_valid_message_directed_at_bot_it_should_send_random_quote()
+        {
+            var botIdentifier = "U3W";
+            var message = new ConsoleApplication.Program.IncomingMessage
+            {
+                text = $"<@{botIdentifier}> some test message",
+                channel = "test-channel"
+            };
+            var getRandomBukowskiQuote = new TestableGetRandomBukowskiQuote();
+            getRandomBukowskiQuote.ExecuteReturnValue = "some random quote";
+            var sendSlackMessage = new TestableSendSlackMessage();
+
+            new MessageHandler(getRandomBukowskiQuote, sendSlackMessage, botIdentifier).Handle(message);
+
+            Assert.That(sendSlackMessage.ExecuteCalledWithChannel, Is.EqualTo(message.channel));
+            Assert.That(sendSlackMessage.ExecuteCalledWithMessage, Is.EqualTo(getRandomBukowskiQuote.ExecuteReturnValue));
+        }
+
+        [Test]
+        public void when_handling_valid_message_not_directed_at_bot_it_should_not_send_random_quote()
+        {
+            var botIdentifier = "U3W";
+            var message = new ConsoleApplication.Program.IncomingMessage
+            {
+                text = $"<@some-other-user> some test message",
+                channel = "test-channel"
+            };
+            var getRandomBukowskiQuote = new TestableGetRandomBukowskiQuote();
+            var sendSlackMessage = new TestableSendSlackMessage();
+
+            new MessageHandler(getRandomBukowskiQuote, sendSlackMessage, botIdentifier).Handle(message);
+
+            Assert.That(sendSlackMessage.ExecuteCalled, Is.EqualTo(false));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        class HelloRTMSession
+        {
+            public string url { get; set; }
+        }
+
+        //TODO: move this somewhere that makes sense
+        public class IncomingMessage
+        {
+            public string type { get; set; }
+            public string user { get; set; }
+            public string text { get; set; }
+            public string channel { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            ConnectToWebsocket().Wait();
+            //Console.ReadKey(); //TODO: stop exiting
+        }
+
+        static async Task ConnectToWebsocket()
+        {
+            var websocketUri = new Uri(await GetWebsocketUrl());
+            var webSocket = new System.Net.WebSockets.ClientWebSocket();
+            await webSocket.ConnectAsync(websocketUri, CancellationToken.None);
+
+            var receiveBytes = new byte[4096];
+            var receiveBuffer = new ArraySegment<byte>(receiveBytes);
+            var transmitted = string.Empty;
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(receiveBuffer, CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "closing websocket", CancellationToken.None);
+                }
+                else
+                {
+                    var messageBytes = receiveBuffer.Skip(receiveBuffer.Offset).Take(result.Count).ToArray();
+
+                    var messageRaw = new UTF8Encoding().GetString(messageBytes);
+                    var message = Newtonsoft.Json.JsonConvert.DeserializeObject<IncomingMessage>(messageRaw);
+
+                    Console.WriteLine(messageRaw);
+                }
+            }
+        }
+
+        static async Task<string> GetWebsocketUrl()
+        {
+            var token = "xoxb-121319484502-bdaKrTkBRlxH17McpyzDZXjq";
+            var startWebsocketUri = "https://slack.com/api/rtm.start";
+            var uri = $"{startWebsocketUri}?token={token}";
+
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(uri))
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<HelloRTMSession>(responseContent).url;
+                }
+            }
+        }
+    }
+}
