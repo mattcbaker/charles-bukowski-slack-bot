17c0fbe46c028309fa06f87443bd617b8f59e29d
Matt Baker <baker.mattc@gmail.com>
2016-12-26 00:47:55 -0700

    remove unused usings

--- SlackSocketConnection.cs
+++ SlackSocketConnection.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using CharlesBukowskiSlackBot;
 
 namespace ConsoleApplication
