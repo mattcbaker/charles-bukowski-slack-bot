2a0fe30b743bdad3fc06c3c5fff9ee8d19eb1509
Matt Baker <baker.mattc@gmail.com>
2017-03-03 18:13:41 -0700

    add try/catch for connection reset

--- src/SlackSocketConnection.cs
+++ src/SlackSocketConnection.cs
@@ -48,48 +48,56 @@ namespace ConsoleApplication
 
         public async void Connect()
         {
-            var websocket = new System.Net.WebSockets.ClientWebSocket();
-            await websocket.ConnectAsync(this.websocketUri, CancellationToken.None);
-
-            new KeepSocketConnectionAlive(websocket).KeepAlive();
-
-            var receiveBytes = new byte[4096];
-            var receiveBuffer = new ArraySegment<byte>(receiveBytes);
-
-            //TODO: move the handling out to the main thread
-            var handler = new MessageHandler(new GetRandomBukowskiQuote(), new SendSlackMessage(websocket),
-                Program.Configuration["slackbot-id"]);
-
-            while (websocket.State == WebSocketState.Open)
+            try
             {
-                var receivedMessage = await websocket.ReceiveAsync(receiveBuffer, CancellationToken.None);
-                if (receivedMessage.MessageType == WebSocketMessageType.Close)
-                {
-                    await
-                        websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "closing websocket",
-                            CancellationToken.None);
-                }
-                else
-                {
-                    var messageBytes = receiveBuffer.Skip(receiveBuffer.Offset).Take(receivedMessage.Count).ToArray();
+                var websocket = new System.Net.WebSockets.ClientWebSocket();
+                await websocket.ConnectAsync(this.websocketUri, CancellationToken.None);
 
-                    var messageRaw = new UTF8Encoding().GetString(messageBytes);
-                    Console.WriteLine(messageRaw);
-                    try
+                new KeepSocketConnectionAlive(websocket).KeepAlive();
+
+                var receiveBytes = new byte[4096];
+                var receiveBuffer = new ArraySegment<byte>(receiveBytes);
+
+                //TODO: move the handling out to the main thread
+                var handler = new MessageHandler(new GetRandomBukowskiQuote(), new SendSlackMessage(websocket),
+                    Program.Configuration["slackbot-id"]);
+
+                while (websocket.State == WebSocketState.Open)
+                {
+                    var receivedMessage = await websocket.ReceiveAsync(receiveBuffer, CancellationToken.None);
+                    if (receivedMessage.MessageType == WebSocketMessageType.Close)
                     {
-                        var message = Newtonsoft.Json.JsonConvert.DeserializeObject<IncomingMessage>(messageRaw);
-
-                        if (message.type == "message")
-                        {
-                            handler.Handle(message);
-                        }
+                        await
+                            websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "closing websocket",
+                                CancellationToken.None);
                     }
-                    catch (Exception)
+                    else
                     {
-                        Console.WriteLine("Unable to process message.");
+                        var messageBytes = receiveBuffer.Skip(receiveBuffer.Offset).Take(receivedMessage.Count).ToArray();
+
+                        var messageRaw = new UTF8Encoding().GetString(messageBytes);
+                        Console.WriteLine(messageRaw);
+                        try
+                        {
+                            var message = Newtonsoft.Json.JsonConvert.DeserializeObject<IncomingMessage>(messageRaw);
+
+                            if (message.type == "message")
+                            {
+                                handler.Handle(message);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Unable to process message.");
+                        }
                     }
                 }
             }
+            catch (System.Exception)
+            {
+                Connect();
+            }
+
         }
     }
 }
